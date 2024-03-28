005b98d6659b12fc71e8d198e7eda3e94ccbdb3b
hylee <hylee@goldenplanet.co.kr>
2024-03-28 10:00:00 +0900

    default setting

---
+++ src/app/IntroduceSetting/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "솜인형 트친소",
+  description: "솜친소 시트를 빠르고 쉽게 갱신해보세요",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/IntroduceSetting/page.tsx
@@ -0,0 +1,9 @@
+const IntroduceSetting = () => {
+  return (
+    <>
+      <div>솜친소 test</div>
+    </>
+  );
+}
+
+export default IntroduceSetting;
